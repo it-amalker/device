7acaae4ce751a296f810b317d2f3ed8dfbb50f53
amalker <it-amalker@yandex.ru>
2019-11-08 12:32:00 +0300

    added script to carousel and services sliders

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@ let writeUsOpen = document.querySelector(".contacts-more-button");
 let writeUsClose = document.querySelector(".write-us-close-button");
 let writeUsPopup = document.querySelector(".modal-write-us");
 
+let carouselSwitches = document.querySelectorAll(".switch");
+let carouselSliders = document.querySelectorAll(".slider");
+
+let serviceButtons = document.querySelectorAll(".service-item");
+let serviceSliders = document.querySelectorAll(".service-slider");
+
 // open/close map popup script
 if (mapPopup) {
   mapOpen.addEventListener("click", function (evt) {
@@ -31,3 +37,28 @@ if (writeUsPopup) {
     writeUsPopup.classList.add("disabled");
   })
 }
+
+// promo carousel sliders and services sliders script
+
+let toggleSliders = function (allSwitches, switchElement, allSliders) {
+  switchElement.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    for (let i = 0; i < allSwitches.length; i++) {
+      allSwitches[i].classList.remove("active");
+      allSliders[i].classList.remove("active");
+    }
+    let switchesArray = Array.prototype.slice.call(allSwitches);
+    let sliderIndex = switchesArray.indexOf(switchElement);
+    switchElement.classList.add("active");
+    allSliders[sliderIndex].classList.add("active");
+  });
+}
+
+let launchToggleSliders = function (switchesList, slidersList) {
+  for (let i = 0; i < switchesList.length; i++) {
+    toggleSliders(switchesList, switchesList[i], slidersList);
+  }
+}
+
+launchToggleSliders(carouselSwitches, carouselSliders);
+launchToggleSliders(serviceButtons, serviceSliders);
